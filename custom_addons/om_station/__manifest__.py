fd066b491b7c1d3af09c1728bb0d1249eb48f7bf
Nurassyl Geroigazy <vip.nurasyl7@gmail.com>
2023-02-28 16:40:18 +0600

    create module station

---
+++ custom_addons/om_station/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Station Management',
+    'version': '1.0.0',
+    'category': 'Station',
+    'summary': 'Station Management system',
+    'description': """This mod""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'post_init_hook': '_synchronize_cron',
+    'license': 'LGPL-3',
+    'sequence': -100,
+    'application': True,
+}
